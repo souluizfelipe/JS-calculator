1b2145a90dcdb9351b85d44e61bd034ea4bf06bf
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-07-27 12:08:18 -0300

    captured clicks on keys and returned on inpDisplay component, captured operator key input click

--- js/main.js
+++ js/main.js
@@ -6,20 +6,36 @@ const Main = {
   },
 
   cacheSelectors: function() {
-    this.display = document.querySelector('#inpDisplay');
-    this.keys = document.querySelectorAll('.key');
+    this.$display = document.querySelector('#inpDisplay');
+    this.$keys = document.querySelectorAll('.key');
+    this.$operators = document.querySelectorAll('.operator');
   },
 
   bindEvents: function() {
-    this.keys.forEach(addEventListener('click', (e) => {
-      const value = e.target.value
-      console.log(value);
-    }));
+    this.$keys.forEach(element => {
+      element.onclick = (this.Events.keys_click).bind(this);
+    });
+
+    this.$operators.forEach(element => {
+      element.onclick = (this.Events.operator_click);
+    });
   },
 
-  Events: function() {
+  Events:{
+    keys_click: function(e) {
+      const value = String(e.target.value);
+      
+      this.$display.value += value
+    },
+    
+    operator_click: function(e) {
+      const value = e.target.value;
+      console.log(value)
+    },
 
   },
+
+  
 };
 
 Main.init();
